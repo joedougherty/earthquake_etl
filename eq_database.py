f3755822f5e691486b36b90252dabdb01c149dfa
Joe Dougherty <joseph.dougherty@gmail.com>
2016-02-18 22:12:55 -0500

    Check if the newly-downloaded csv 1.) contains records and 2.) contains _newer_ records than the latest record in the db before attempting to append to the pre-existing dataset.

--- eq_database.py
+++ eq_database.py
@@ -7,7 +7,7 @@ Data types and any add'l metadata from:
 http://earthquake.usgs.gov/earthquakes/feed/v1.0/glossary.php 
 """
 
-def get_db_connection(db_name):
+def get_db(db_name):
     """ 
     Create a DB connection to the table passed in by db_name. 
     Returns a namedtuple containing the connection object and the DB cursor.
@@ -19,5 +19,6 @@ def get_db_connection(db_name):
 
 def create_eq_table(csv_file, db_location):
     dataframe = pd.read_csv(csv_file)
-    db = get_db_connection(db_location)
-    dataframe.to_sql('all_earthquakes', db.conn)
+    db = get_db(db_location)
+    # Careful! This will replace an existing db.
+    dataframe.to_sql('all_earthquakes', db.conn, if_exists='replace', index=False)
--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,14 @@
+# System-level modules
 from fabric.api import task, env, local
 from tinyetl import TinyETL
 import os
 import requests
 import sqlite3
-from eq_database import create_eq_table 
+import pandas as pd
+import iso8601
+
+# Helper script
+from eq_database import create_eq_table, get_db
 
 description = """
 Real-time Earthquake Data 
@@ -16,6 +21,8 @@ etl = TinyETL(
     env=env, 
     log_dir=os.path.join(os.getcwd(), "logs"),
     tmpdata_dir=os.path.join(os.getcwd(), "download_data"),
+    # Non-required args, but good for namespacing purposes
+    db_location = os.path.join(os.getcwd(), 'database', 'earthquakes.sqlite'),
 )
 
 #-----------------------------#
@@ -30,14 +37,29 @@ def create_required_directories():
 @task
 def create_initial_database():
     """ Create initial database for real-time earthquake data. """
-    if not etl.dry_run: # TODO is this strictly necessary?
-        etl.database_dir = os.path.join(os.getcwd(), 'database') 
-        create_eq_table(os.path.join(etl.database_dir, 'earthquakes.sqlite'))
+    all_earthquakes = os.path.join(os.getcwd(), 'download_data', 'all_earthquakes', 'all_month.csv')
+    create_eq_table(all_earthquakes, etl.db_location)
+
+@task
+def info():
+    """ Print some useful information about this ETL process. """
+    setup()
+    print(etl)
 
 @etl.log
-def set_csv_file_location():
-    if not etl.dry_run: # TODO is this strictly necessary? If so, document _why_!
-        etl.csv_location = os.path.join(etl.tmpdata_dir, 'eq_{}.csv'.format(etl.timestamp()))
+def setup():
+    """ This is a good place for any runtime additions to the etl object. """
+    etl.csv_file_location = os.path.join(etl.tmpdata_dir, 'eq_{}.csv'.format(etl.timestamp()))
+    etl.db = get_db(etl.db_location)
+
+@etl.log
+def update_data_is_newer(latest_prod_date, earlier_update_date):
+    latest_production_time = iso8601.parse_date(latest_prod_date)
+    earliest_new_data_time = iso8601.parse_date(earliest_update_date)
+    
+    if earliest_new_data_eq_time > latest_production_time:
+        return True
+    return False
 
 #-----------------------------#
 # ETL Tasks                   #
@@ -50,8 +72,33 @@ def download_data():
     etl.download_file(endpoint, etl.csv_file_location)
 
 @task
+@etl.log
+def append_newest_data():
+    """ Parse the newest data from USGS and append it to the final dataset. """
+    df = pd.read_csv(etl.csv_file_location)
+    num_new_rows = len(df)
+
+    earliest_new_data_time = df.time.min()
+    latest_prod_date_result = etl.db.cursor.execute('select max(time) from all_earthquakes')
+    latest_prod_date = latest_prod_date_result.fetchone()[0]
+
+    # Verify that the downloaded records are actually new 
+    # with respect to the previously existing data
+    if num_new_rows > 0 and update_data_is_newer(latest_prod_date, earliest_new_data_time):
+        new_records = [tuple(x) for x in df.to_records(index=False)]
+        insert_query = "INSERT INTO all_earthquakes VALUES" \
+                       "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+
+        # Write new rows to the database
+        etl.db.cursor.executemany(insert_query, new_records) 
+        etl.logger.info("Wrote {} rows to database.".format(num_new_rows))
+    else:
+        etl.logger.info("{} contains no new rows. Process ends here.".format(etl.csv_file_location))
+
+@task
 def main():
     """ Run the whole ETL process. """
-    set_csv_file_location()
+    setup()
     download_data()
-
+    append_newest_data()
+   
